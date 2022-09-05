5b4316be33c3c4e8408ec63a4729e265ff29ae95
ju0song <cuobalee@naver.com>
2022-09-05 21:50:17 +0900

    1.문자찾기

---
+++ src/String/Findtext.java
@@ -0,0 +1,29 @@
+package String;
+
+import java.util.Scanner;
+
+public class Findtext {
+    public static void main(String[] args) {
+        Findtext findtext =new Findtext();
+        Scanner a =new Scanner(System.in);
+        String s = a.next();
+        char c = a.next().charAt(0);
+        System.out.println(findtext.solution(s,c));
+
+    }
+    public int solution(String s,char c){
+        int answer =0;
+        s=s.toUpperCase();//대문자로 다 만들어주는 함수
+        c=Character.toUpperCase(c);
+
+        //string의 길이 length
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == c)
+            answer++;
+        }
+
+        return answer;
+
+
+    }
+}
