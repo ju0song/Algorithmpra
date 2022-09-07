125ed52494e6bf114761bb9b6e2a523984342bb1
ju0song <cuobalee@naver.com>
2022-09-07 23:10:49 +0900

    대소문자변환

---
+++ src/String/Caseconversion.java
@@ -0,0 +1,33 @@
+package String;
+import java.util.*;
+
+public class Caseconversion {
+
+
+    public static void main(String[] args) {
+        Caseconversion caseconversion = new Caseconversion();
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.next();
+        System.out.println(caseconversion.solution(a));
+
+
+    }
+
+    public String solution(String a) {
+        StringBuilder answer = new StringBuilder();
+        for (char x : a.toCharArray()) {//toCharArray 란 String문자열을 char형 배열로 바꿔주는 메소드
+            if (Character.isLowerCase(x)) {//isLowerCase x가 소문자인지 확인
+                answer.append(Character.toUpperCase(x));
+            } else {
+                answer.append(Character.isLowerCase(x));
+            }
+
+        }
+        return answer.toString();
+    }
+}
+            //이제 대문자로 바꾸기
+
+
+
+
