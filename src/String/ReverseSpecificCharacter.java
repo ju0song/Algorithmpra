d5274f70199a6abd483f8c6ac583c6f817585adc
ju0song <cuobalee@naver.com>
2022-09-22 10:20:58 +0900

    특정문자 뒤집기

---
+++ src/String/ReverseSpecificCharacter.java
@@ -0,0 +1,31 @@
+package String;
+
+import java.util.Scanner;
+
+public class ReverseSpecificCharacter {
+    public String solution(String str){
+        String answer;
+        char[] s=str.toCharArray();
+        int lt=0, rt=str.length()-1;
+        while(lt<rt){
+            if(!Character.isAlphabetic(s[lt])) lt++;
+            else if(!Character.isAlphabetic(s[rt])) rt--;
+            else{
+                char tmp=s[lt];
+                s[lt]=s[rt];
+                s[rt]=tmp;
+                lt++;
+                rt--;
+            }
+        }
+        answer=String.valueOf(s);
+        return answer;
+    }
+
+    public static void main(String[] args){
+        ReverseSpecificCharacter T = new ReverseSpecificCharacter();
+        Scanner kb = new Scanner(System.in);
+        String str=kb.next();
+        System.out.println(T.solution(str));
+    }
+}
