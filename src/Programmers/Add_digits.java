dbe739f80e492b49ef28b29ec24d7c400050953b
ju0song <cuobalee@naver.com>
2022-10-09 21:05:10 +0900

    자연수 더하기

--- src/Programmers/Add_digits.java
+++ src/Programmers/Add_digits.java
@@ -1,14 +1,14 @@
+package Programmers;
+
 public class Add_digits {
         public int solution(int n) {
-
             String str=String.valueOf(n);
             String[] array = str.split("");
             int answer = 0;
 
-            for(int i=0;i<array.length;i++) {
-                answer += Integer.parseInt(array[i]);
+            for (String s : array) {
+                answer += Integer.parseInt(s);
             }
-
             return answer;
         }
 
@@ -16,5 +16,5 @@ public class Add_digits {
         Add_digits add_digits = new Add_digits();
         System.out.println(add_digits.solution(128));
     }
-    }
+}
 
