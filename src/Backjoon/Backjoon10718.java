19e28e61c53f8ea5ffcd614c5b435909e31e84f6
ju0song <cuobalee@naver.com>
2022-10-11 19:47:32 +0900

    숫자 문자열과 영단어 백준10718

---
+++ src/Backjoon/Backjoon10718.java
@@ -0,0 +1,10 @@
+package Backjoon;
+
+public class Backjoon10718 {
+    public static void main(String[] args) {
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append("강한친구 대한육군\n");
+        stringBuffer.append("강한친구 대한육군");
+        System.out.println(stringBuffer);
+    }
+}
---
+++ src/Programmers/NumberStringAndEnglishwords/Solution.java
@@ -0,0 +1,32 @@
+package Programmers.NumberStringAndEnglishwords;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+//https://school.programmers.co.kr/learn/courses/30/lessons/81301
+//다음은 숫자의 일부 자릿수를 영단어로 바꾸는 예시입니다.
+//1478 → "one4seveneight"
+//234567 → "23four5six7"
+//10203 → "1zerotwozero3"
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String[] arr ={"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        String[] number = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        for(int i =0;i<10;i++){
+            s = s.replace(arr[i],number[i]);
+        }
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution("23four5six7"));
+    }
+}
+
+
+
+
+
