1540ac1f9e98b80a57517a937241aae491eab2e6
ju0song <cuobalee@naver.com>
2022-10-17 21:08:18 +0900

    두 정수 사이의 합

---
+++ src/Programmers/두정수사이의합/Solution.java
@@ -0,0 +1,25 @@
+package Programmers.두정수사이의합;
+
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        if(a <b) {
+            for (int A = a; A < b + 1; A++) {
+                answer += A;
+            }
+        }else if(b<a){
+            for (int B = b; B < a + 1; B++) {
+                answer += B;
+            }
+        }else {
+            answer +=a;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+       Solution solution = new Solution();
+        System.out.println(solution.solution(3,5));
+        System.out.println(solution.solution(5,3));
+    }
+}
