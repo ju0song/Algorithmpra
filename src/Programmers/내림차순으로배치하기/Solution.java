6f08da83327f65a860ac38de75c2f6de6a0f76c5
ju0song <cuobalee@naver.com>
2022-10-24 19:47:48 +0900

    내림차순으로 배치하기

---
+++ src/Programmers/내림차순으로배치하기/Solution.java
@@ -0,0 +1,27 @@
+package Programmers.내림차순으로배치하기;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+class Solution {
+    public long solution(long n) {
+
+        String[] str =String.valueOf(n).split("");
+        Arrays.sort(str);
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String s : str) {
+            stringBuilder.append(s);
+        }
+        stringBuilder.reverse();
+        long answer =Long.parseLong(stringBuilder.toString());
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(118372));
+    }
+}
