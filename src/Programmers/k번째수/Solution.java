90ce30a3f8e77f8fb57e09eb04f607bb1cc396f5
ju0song <cuobalee@naver.com>
2022-11-05 14:12:33 +0900

    k번째수

---
+++ src/Programmers/k번째수/Solution.java
@@ -0,0 +1,19 @@
+package Programmers.k번째수;
+
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        if (Math.sqrt(n) % 1 == 0) {
+            answer = (long) (Math.pow(Math.sqrt(n) + 1, 2));
+        } else {
+            answer = -1;
+        }
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(121));
+    }
+}
