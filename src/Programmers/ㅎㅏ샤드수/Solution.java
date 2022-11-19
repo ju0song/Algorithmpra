4554784e65cf93c92bd668b27f952b2b3e33bec9
ju0song <cuobalee@naver.com>
2022-11-19 20:00:06 +0900

    하샤드수

---
+++ src/Programmers/ㅎㅏ샤드수/Solution.java
@@ -0,0 +1,20 @@
+package Algorithmpra.src.Programmers.ㅎㅏ샤드수;
+
+class Solution {
+    public boolean solution(int x) {
+        int n = x;
+        int result = 0;
+
+        while(n > 10){
+            result += n % 10;
+            n /= 10;
+        }
+        result += n;
+        return x % result == 0;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(10));
+    }
+}
