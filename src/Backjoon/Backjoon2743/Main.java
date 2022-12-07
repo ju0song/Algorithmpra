ffee762b62ac58cc32cad74bde861473f712af8b
ju0song <cuobalee@naver.com>
2022-12-07 19:12:34 +0900

    백준2743

---
+++ src/Backjoon/Backjoon2743/Main.java
@@ -0,0 +1,13 @@
+package Backjoon.Backjoon2743;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    char[] charArr = br.readLine().toCharArray();
+    System.out.println(charArr.length);
+  }
+}
