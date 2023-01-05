919a0fd64b89e5277b0f1194809f345efa2af3ec
ju0song <cuobalee@naver.com>
2023-01-05 17:29:37 +0900

    행렬의 곱셈

---
+++ src/Programmers/행렬의곱셈/Solution.java
@@ -0,0 +1,24 @@
+package Programmers.행렬의곱셈;
+
+import java.util.Arrays;
+
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2[0].length; j++) {
+                for (int k = 0; k < arr1[0].length; k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] arr1 = {{1, 4}, {3, 2}, {4, 1}};
+        int[][] arr2 = {{3, 3}, {3, 3},};
+        System.out.println(Arrays.deepToString(solution.solution(arr1, arr2)));
+    }
+}
